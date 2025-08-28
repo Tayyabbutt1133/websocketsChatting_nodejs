f4258b2b7065b39018562a4ba37f814f15be86cb
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-08-28 13:08:54 +0500

    server file updated

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require('express')
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 // Serve static files from "public"
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,13 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-// Start server
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-
 
 
 const io = require('socket.io')(server);
